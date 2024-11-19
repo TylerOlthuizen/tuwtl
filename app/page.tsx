b4b66e7a3ae0ed0b59332acd4f8f2d514f08586a
Tyler Olthuizen <tylerolthuizen@gmail.com>
2024-11-18 22:31:56 -0700

    more style changes.

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { FaTelegramPlane } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="p-8 pb-0 site-background">
-      <main className="min-h-screen p-8 relative container mx-auto">
-        <nav className="absolute top-0 left-0">
+    <div className="p-8 xl:p-10 pb-0 site-background">
+      <main className="min-h-screen relative mx-auto">
+        <nav className="absolute top-4 left-0">
           <ul className="flex gap-8 flex-row items-center nav-list">
             <li>
               <a
@@ -28,7 +28,7 @@ export default function Home() {
             </li>
           </ul>
         </nav>
-        <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <a className="cta" target="_blank" href="https://x.com/tuwtl">
             x.com/tuwtl
           </a>
